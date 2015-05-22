6fc218071c57f9c4b9c9129444fb616cc4b62f6a
Tim Lenardo <lenardot@fb.com>
2015-05-21 18:25:52 -0700

    Initial Commit

---
+++ Mirror/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mirror
+//
+//  Created by Tim Lenardo on 5/21/15.
+//  Copyright (c) 2015 tldesign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MirrorTests/MirrorTests.m
@@ -0,0 +1,40 @@
+//
+//  MirrorTests.m
+//  MirrorTests
+//
+//  Created by Tim Lenardo on 5/21/15.
+//  Copyright (c) 2015 tldesign. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MirrorTests : XCTestCase
+
+@end
+
+@implementation MirrorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
